be5db597232e2c2529419d5ef4342d9cd45f3566
jks6404 <jannusingh.75@gmail.com>
2023-08-27 06:34:27 +0530

    submitting code-Sun Aug 27 2023 06:34:25 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const totalFruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+
+  let [fruits,setfruits] = useState(totalFruits);
+  console.log(fruits);
+
+  const showSuggestion= (e)=>{
+    let filteredFruits = [];
+    filteredFruits = totalFruits.filter((value)=>{
+      return value.toLowerCase().includes(e.target.value);
+    })
+    setfruits(filteredFruits);
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        {/* <h1>Hello...</h1>b */}
+        <h1>Search item</h1>
+        <input type="text" onChange={(e)=>showSuggestion(e)}/>
+        <ul>
+          {      
+            fruits.map((value,index)=>(
+              <li key={index}>{value}</li>
+            ))
+          }
+        </ul>
     </div>
   )
 }
